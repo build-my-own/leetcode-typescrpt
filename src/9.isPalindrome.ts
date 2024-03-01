08906b32f5d7edad07232b986140a47f797cf844
pork-sashimi <travischen1996@gmail.com>
2024-03-01 09:48:38 +0800

    it's an automatic commit

--- src/9.isPalindrome.ts
+++ src/9.isPalindrome.ts
@@ -3,12 +3,33 @@
  *
  * [9] 回文数
  * 
- * 时间复杂度：O(n) n 是数字的位数
+ * 时间复杂度：O(log(n)) n 是数字的大小
  * 空间复杂度：O(1)
  */
 
 // @lc code=start
 function isPalindrome(x: number): boolean {
+    if (x >= 0 && x < 10) {
+        return true;
+    }
+    if (x < 0 || x % 10 === 0) {
+        return false;
+    }
+    let r = 0;
+    let l = x;
+    while (r < l) {
+        r = r * 10 + l % 10;
+        l = Math.floor(l / 10);
+    }
+    return l === r || l === Math.floor(r / 10);
+};
+// @lc code=end
+
+/**
+ * 时间复杂度 O(n) n 是数字的位数
+ * 空间复杂度 O(1)
+ */
+function isPalindrome1(x: number): boolean {
     if (x < 0) {
         return false;
     }
@@ -23,4 +44,3 @@ function isPalindrome(x: number): boolean {
     }
     return r === x;
 };
-// @lc code=end
