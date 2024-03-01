43e83edd89b81d0c1412020e5efc6109e38dec3d
pork-sashimi <travischen1996@gmail.com>
2024-03-01 09:10:20 +0800

    it's an automatic commit

---
+++ src/9.isPalindrome.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=typescript
+ *
+ * [9] 回文数
+ * 
+ * 时间复杂度：O(n) n 是数字的位数
+ * 空间复杂度：O(1)
+ */
+
+// @lc code=start
+function isPalindrome(x: number): boolean {
+    if (x < 0) {
+        return false;
+    }
+    if (x < 10) {
+        return true;
+    }
+    let r = 0;
+    let n = x;
+    while (n > 0) {
+        r = r * 10 + n % 10;
+        n = Math.floor(n / 10);
+    }
+    return r === x;
+};
+// @lc code=end
